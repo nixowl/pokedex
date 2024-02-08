b502b791e30b449780821b9ec2ebd9e0dccae923
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-02-08 13:35:14 +0100

    create separate components for header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import { ThemeToggle } from "./ui/ThemeToggle";
+
+function Footer() {
+  return (
+    <footer className="bg-card flex justify-between p-4 rounded-md m-2">
+      {/* Screen size indicators */}
+      <div className="flex flex-row z-50 size-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+        <div className="block sm:hidden">xs</div>
+        <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">
+          sm
+        </div>
+        <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">md</div>
+        <div className="hidden lg:block xl:hidden 2xl:hidden">lg</div>
+        <div className="hidden xl:block 2xl:hidden">xl</div>
+        <div className="hidden 2xl:block">2xl</div>
+          </div>
+          
+          <ThemeToggle />
+    </footer>
+  );
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+      <header className="bg-card p-3 rounded-md mb-3">hello im a header</header>
+    );
+  
+}
+
+export default Header;
