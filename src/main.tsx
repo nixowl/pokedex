53a0acc9fa8976b69946df21593e9b1585fde0ac
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-02-08 13:33:51 +0100

    add theme provider for dark mode toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 import { NextUIProvider } from "@nextui-org/react";
-import './globals.css'
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
+    <NextUIProvider className="flex flex-col h-full">
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <App />
+      </ThemeProvider>
     </NextUIProvider>
   </React.StrictMode>
 );
