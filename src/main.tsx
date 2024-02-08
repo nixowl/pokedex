10c84f0d21632b1022e39a6786b2e1a6cfbab883
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-02-08 10:10:24 +0100

    install and import nextui provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
+import { NextUIProvider } from "@nextui-org/react";
 import './globals.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </React.StrictMode>
+);
